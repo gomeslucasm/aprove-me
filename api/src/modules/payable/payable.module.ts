1611415cdd6478ef59250aab7bfa6199107f4178
Lucas Muller Gomes <lmgomes96@gmail.com>
2024-05-22 22:18:50 -0300

    fix: register prisma module

--- api/src/modules/payable/payable.module.ts
+++ api/src/modules/payable/payable.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PayableController } from './payable.controller';
 import { PayableService } from './payable.service';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PayableController],
-  providers: [PayableService]
+  providers: [PayableService],
 })
 export class PayableModule {}
